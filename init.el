ee5c5fbd749d182adca170a15809ae95e261778f
Eric Haberstroh <gpg@erixpage.de>
2021-03-31 15:02:56 +0200

    Add Treemacs and some devel settings

--- init.el
+++ init.el
@@ -233,6 +233,47 @@
 (use-package forge
   :after magit)
 
+;; Treemacs
+(use-package treemacs
+  :bind
+  (("M-0" . treemacs-select-window)
+   ("C-x t 1" . treemacs-delete-other-windows)
+   ("C-x t t" . treemacs)
+   ("C-x t B" . treemacs-bookmark)
+   ("C-x t C-t" . treemacs-find-file)
+   ("C-x t M-t" . treemacs-find-tag)))
+
+(use-package treemacs-projectile
+  :after (treemacs projectile))
+
+(use-package treemacs-icons-dired
+  :after (treemacs dired)
+  :config (treemacs-icons-dired-mode))
+
+(use-package treemacs-magit
+  :after (treemacs magit))
+
+;; Development
+
+;; Enable electric-pair-mode in all prog-mode buffers
+(add-hook 'prog-mode-hook 'electric-pair-mode)
+
+;; Language Server Protocol (LSP)
+(use-package lsp-mode
+  :hook ((php-mode . lsp)
+	 (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp
+  :init
+  (setq lsp-keymap-prefix "C-c x"))
+
+;; LSP enhancements
+(use-package lsp-ui :commands lsp-ui-mode)
+(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
+(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
+
+;; PHP
+(use-package php-mode)
+
 ;; Use <pause> key to save the current buffer
 (global-set-key (kbd "<pause>")
 		'save-buffer)
