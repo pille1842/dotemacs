5244bab4793b3098734af46e9e3fec0fb06f94f2
Eric Haberstroh <gpg@erixpage.de>
2021-03-31 18:01:54 +0200

    Add custom file to tracked files

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ange-ftp-netrc-filename "~/.authinfo.gpg" t)
+ '(package-selected-packages
+   '(php-mode forge magit counsel-projectile projectile visual-fill-column org-bullets org-plus-contrib helpful which-key ivy-rich counsel ivy smooth-scrolling rainbow-delimiters doom-modeline fira-code-mode doom-themes use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
