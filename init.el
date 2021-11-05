d97196d34da5d0054e4dd31c8807d7456f55d67d
Eric Haberstroh <gpg@erixpage.de>
2021-11-05 12:22:51 +0100

    Initial commit of init.el

--- init.el
+++ init.el
@@ -1,94 +1,99 @@
-;;; init.el --- Emacs configuration
-;;
+;;; init.el --- Emacs initialization file -*- lexical-binding: t -*-
+
 ;; Copyright (C) 2021 Eric Haberstroh
-;;
-;; Filename:    init.el
-;; Description: Configure Emacs to my taste
-;; Author:      Eric Haberstroh <gpg@erixpage.de>
-;; Homepage:    https://github.com/pille1842/dotemacs
-;; Version:     1.0.0
-;; Keywords:    initialization startup
-;; GitHub:      https://github.com/pille1842/dotemacs
 
-;; This file is not part of GNU Emacs
+;; Maintainer: gpg@erixpage.de
+;; Keywords: startup init
+;; Package: eh-emacs
 
-;;; License:
-;;
-;; This program is free software; you can redistribute it and/or
-;; modify it under the terms of the GNU General Public License as
-;; published by the Free Software Foundation; either version 3 of the
-;; License, or (at your option) any later version.
-;;
-;; This program is distributed in the hope that it will be useful, but
-;; WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-;; General Public License for more details.
-;;
+;; This file is NOT part of GNU Emacs.
+
+;; This file is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
 ;; You should have received a copy of the GNU General Public License
-;; along with this program; if not, see
-;; <https://www.gnu.org/licenses>.
+;; along with this file.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
-;; The purpose of this file is to prepare Emacs for loading the actual
-;; configuration, which resides in erix-emacs.org in this directory.
-;;
-;; See the repo: https://github.com/pille1842/dotemacs
+;; This file configures Emacs.
 
 ;;; Code:
 
-;; Initialize the package system and use-package
+(setq inhibit-startup-message t)
+
+(scroll-bar-mode -1)    ;; Disable visible scrollbar
+(tool-bar-mode -1)      ;; Disable the toolbar
+(tooltip-mode -1)       ;; Disable tooltips
+(menu-bar-mode -1)      ;; Disable the menu bar
+(set-fringe-mode 10)    ;; Give some breathing room
+
+;; Set up the visible bell
+(setq visible-bell t)
+
+;; Use a better-looking font
+(set-face-attribute 'default nil :font "Fira Code Retina" :height 120)
+
+;; White background blinds me
+(load-theme 'tango-dark t)
+
+;; Make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+;; Don't put customizations in this file
+(setq custom-file (concat user-emacs-directory "custom.el"))
+(when (file-exists-p custom-file)
+  (load custom-file))
+
+;; Initialize package sources
 (require 'package)
+
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("org"   . "https://orgmode.org/elpa/")
-                         ("elpa"  . "https://elpa.gnu.org/packages/")))
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
+
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
 
-(unless (package-installed-p "use-package")
+;; Initialize use-package
+(unless (package-installed-p 'use-package)
   (package-install 'use-package))
+
+(require 'use-package)
 (setq use-package-always-ensure t)
 
-;; Prepare loading erix-emacs.org
-(defvar erix-emacs-configuration-main-file "erix-emacs"
-  "Base name of the main configuration file.")
+;; Configure searching
+(use-package swiper)
 
-(defun erix-emacs--expand-file-name (file extension)
-  "Return canonical path to FILE with EXTENSION."
-  (expand-file-name
-   (concat user-emacs-directory file extension)))
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+	 :map ivy-minibuffer-map
+	 ("TAB" . ivy-alt-done)
+	 ("C-l" . ivy-alt-done)
+	 ("C-j" . ivy-next-line)
+	 ("C-k" . ivy-previous-line)
+	 :map ivy-switch-buffer-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-l" . ivy-done)
+	 ("C-d" . ivy-switch-buffer-kill)
+	 :map ivy-reverse-i-search-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
 
-(defun erix-emacs-load-config ()
-  "Load main Emacs configuration, either '.el' or '.org' file."
-  (let* ((main-init erix-emacs-configuration-main-file)
-	 (main-init-el (erix-emacs--expand-file-name main-init ".el"))
-	 (main-init-org (erix-emacs--expand-file-name main-init ".org")))
-    (require 'org)
-    (if (file-exists-p main-init-el)
-	(load-file main-init-el)
-      (when (file-exists-p main-init-org)
-	(org-babel-load-file main-init-org)))))
-
-(erix-emacs-load-config)
-
-(declare-function org-babel-tangle-file "ob-tangle")
-
-(defun erix-emacs-build-config ()
-  "Produce Emacs Lisp init from the Org dotemacs file.
-Add this to `kill-emacs-hook', to use the newest file in the next
-session. The idea is to reduce startup time, though just by
-rolling it over to the end of a session rather than the beginning
-of it."
-  (let* ((main-init erix-emacs-configuration-main-file)
-	 (main-init-el (erix-emacs--expand-file-name main-init ".el"))
-	 (main-init-org (erix-emacs--expand-file-name main-init ".org")))
-    (when (file-exists-p main-init-el)
-      (delete-file main-init-el))
-    (require 'org)
-    (when (file-exists-p main-init-org)
-      (org-babel-tangle-file main-init-org main-init-el))))
-
-(add-hook 'kill-emacs-hook #'erix-emacs-build-config)
+;; Configure the modeline
+(use-package doom-modeline
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 30)))
 
 ;;; init.el ends here
