04006f670395ce3ba03772582424dfac4f13adb0
Eric Haberstroh <gpg@erixpage.de>
2021-11-06 18:14:53 +0100

    Systemcrafters, Episode 4

--- init.el
+++ init.el
@@ -221,4 +221,29 @@
 (ehmacs/leader-keys
   "ts" '(hydra-text-scale/body :which-key "scale text"))
 
+(setq auth-sources '("~/.authinfo.gpg"))
+
+;; Project management
+(use-package projectile
+  :config (projectile-mode)
+  :custom (projectile-completion-system 'ivy)
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  (when (file-directory-p "~/Code")
+    (setq projectile-project-search-path '("~/Code")))
+  (setq projectile-switch-project-action #'projectile-dired))
+
+(use-package counsel-projectile
+  :config (counsel-projectile-mode 1))
+
+;; Version control
+(use-package magit
+  :commands (magit-status magit-get-current-branch)
+  :custom
+  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+
+(use-package forge
+  :after magit)
+
 ;;; init.el ends here
