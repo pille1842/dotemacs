a42e6d700faad595f818efa3fb8b3abf38ab5be7
Eric Haberstroh <eric@mac.speedport.ip>
2025-09-07 17:42:40 +0200

    Add license and basic commentary to all files

--- early-init.el
+++ early-init.el
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 ;;; early-init.el --- Early Startup File for Emacs Configuration of Eric Haberstroh -*- lexical-binding: t -*-
 
 ;; Copyright (C) 2023 Eric Haberstroh <eric@erichaberstroh.de>
@@ -26,3 +27,34 @@
 (add-to-list 'initial-frame-alist '(fullscreen . maximized))
 (setenv "LIBRARY_PATH" "/opt/homebrew/lib/gcc/current:/opt/homebrew/lib/gcc/current/gcc/aarch64-apple-darwin24/15")
 >>>>>>> 69277aa (Initial commit)
+=======
+;;; early-init.el --- Emacs Early Initialization File -*- lexical-binding: t -*-
+
+;; Copyright (C) 2025 Eric Haberstroh
+
+;; Author: Eric Haberstroh <eric@erichaberstroh.de>
+;; Maintainer: Eric Haberstroh <eric@erichaberstroh.de>
+;; Created: 2025
+;; Version: 1.0
+;; Package-Requires: ((emacs "30.2") (compat "30"))
+;; URL: https://github.com/pille1842/dotemacs
+;; Keywords: initialization, configuration
+
+;; This file is NOT part of GNU Emacs.
+
+;; Emacs Configuration © 2025 by Eric Haberstroh is licensed under CC BY-SA 4.0.
+;; To view a copy of this license, visit:
+;; <https://creativecommons.org/licenses/by-sa/4.0/>
+
+;;; Commentary:
+
+;; This file's job is to do early configuration steps which must be done
+;; before any frames in the graphical environment are created.
+
+;;; Code:
+
+(add-to-list 'initial-frame-alist '(fullscreen . maximized))
+(setenv "LIBRARY_PATH" "/opt/homebrew/lib/gcc/current:/opt/homebrew/lib/gcc/current/gcc/aarch64-apple-darwin24/15")
+
+;;; early-init.el ends here
+>>>>>>> 321bdef (Add license and basic commentary to all files)
--- init.el
+++ init.el
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 ;;; init.el --- Emacs Configuration of Eric Haberstroh -*- lexical-binding: t -*-
 
 ;; Copyright (C) 2023 Eric Haberstroh <eric@erichaberstroh.de>
@@ -217,3 +218,36 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (org-babel-load-file (concat user-emacs-directory "config.org"))
 >>>>>>> 69277aa (Initial commit)
+=======
+;;; init.el --- Emacs Initialization File -*- lexical-binding: t -*-
+
+;; Copyright (C) 2025 Eric Haberstroh
+
+;; Author: Eric Haberstroh <eric@erichaberstroh.de>
+;; Maintainer: Eric Haberstroh <eric@erichaberstroh.de>
+;; Created: 2025
+;; Version: 1.0
+;; Package-Requires: ((emacs "30.2") (compat "30"))
+;; URL: https://github.com/pille1842/dotemacs
+;; Keywords: initialization, configuration
+
+;; This file is NOT part of GNU Emacs.
+
+;; Emacs Configuration © 2025 by Eric Haberstroh is licensed under CC BY-SA 4.0.
+;; To view a copy of this license, visit:
+;; <https://creativecommons.org/licenses/by-sa/4.0/>
+
+;;; Commentary:
+
+;; This file's job is to initialize the Emacs package system and then load
+;; the actual configuration, which is stored as literate code blocks in
+;; config.org.
+
+;;; Code:
+
+(package-initialize)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(org-babel-load-file (concat user-emacs-directory "config.org"))
+
+;;; init.el ends here
+>>>>>>> 321bdef (Add license and basic commentary to all files)
